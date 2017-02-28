09e23a54f8fa959628e7b7b23a118d5a5fc82fce
alpeshvas <alpeshvash@gmail.com>
2017-02-28 11:06:01 +0530

    Script for deleting unnecessary mails in iitg webmail

---
+++ 1.js
@@ -0,0 +1,32 @@
+var table=document.getElementsByTagName("table")[7];
+var extra=["noticeboard","sports","security","patel","medsec","maintenance","mailman-owner",
+"gtm","estb","engoff","elections","dos","arun","akb","vp@iitg","techniche","swc@","pro@i","placement@","clhod@","hackers@",
+"techsec","gensec_welfare@","sport","cultsec","edc","alcheringa"];
+function check(withthis){
+	for(var i=0,len=extra.length;i<len;++i){
+		if(withthis.startsWith(extra[i])){
+			return true;
+		}
+	}
+	return false;
+}
+for ( var i=0;row=table.rows[i];i++){
+		var col=row.cells[1];
+		// console.log(col);
+		if(col!=null){
+
+			if(col.getElementsByTagName("label")[0]!=null){
+				var title=col.getAttribute("title");
+				var id=col.getElementsByTagName("label")[0].getAttribute("for");
+				if(check(title)){
+					document.getElementById(id).checked=true;
+					// console.log(title);
+				}
+			}
+		}
+
+		// title;
+	// console.log("done this ")
+}
+;
+document.forms[0].submit();
---
+++ run.js
@@ -0,0 +1,15 @@
+var table=document.getElementsByTagName("table")[7];
+for ( var i=0;row=table.rows[i],i<100;i++){
+		var col=row.cells[1];
+		// console.log(col);
+		if(col!=null){
+
+			var title=col.getAttribute("title"));
+			if(col.getElementsByTagName("label")[0]!=null)
+				console.log(col.getElementsByTagName("label")[0].getAttribute("for"));
+		}
+
+		// title;
+	// console.log("done this ")
+}
+;
